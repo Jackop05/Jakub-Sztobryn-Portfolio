a806ab03d19995ce6180ba778cb9b3b378c50c64
Jackop05 <jakubnyrbotzs@gmail.com>
2024-08-27 10:25:31 +0200

    Small bug fixes

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -11,6 +11,70 @@ const projectData = [
         name: 'Allegro clone',
         technologies: ['Vite.js', 'Node.js', 'MongoDB', 'Tailwind'],
         description: 'Allegro is a website where you can buy everything and i have built version of it myself',
+        image: '/images/allegro-app-preview.png',
+    },
+    {
+        iconIndex: 2,
+        name: 'Messenger clone',
+        technologies: ['Vite.js', 'Node.js', 'MongoDB', 'Tailwind'],
+        description: 'Messenger is one of most popular chat apps in the world and i have coded it myself',
+        image: '/images/messenger-app-preview.png',
+    },
+    {
+        iconIndex: 3,
+        name: 'Maze game',
+        technologies: ['Vite.js', 'Tailwind', 'BFS algorithm'],
+        description: 'A game where you can find shortest path to exit a maze using BFS algorithm.',
+        image: '/images/mazeGame-app-preview.png.png',
+    },
+    {
+        iconIndex: 4,
+        name: 'Receipt app',
+        technologies: ['React.js', 'Tailwind', 'API calls'],
+        description: 'Find your favorite recipes using an easy-to-navigate interface.',
+        image: '/images/receipt-app-preview.png',
+    },
+    {
+        iconIndex: 5,
+        name: '3D shirts shop',
+        technologies: ['React.js', 'Node.js', 'Three.js', 'GSAP animations', 'Tailwind', 'OpenAi API'],
+        description: 'An interactive 3D shop where users can design and purchase custom shirts.',
+        image: '/images/customShirt-app-preview.png',
+    },
+    {
+        iconIndex: 6,
+        name: 'Apple clone',
+        technologies: ['React.js', 'Three.js', 'GSAP animations', 'Tailwind'],
+        description: 'A clone of the Apple website with smooth animations and sleek design.',
+        image: '/images/apple-app-preview.png',
+    },
+    {
+        iconIndex: 7,
+        name: 'Weather app',
+        technologies: ['React.js', 'API calls', 'Tailwind'],
+        description: 'A weather forecasting app providing real-time data with a clean UI.',
+        image: '/images/weather-app-preview.png',
+    },
+];
+
+export default projectData;
+
+
+/*
+
+const projectData = [
+    {
+        iconIndex: 0,
+        name: 'Korepetycje Online',
+        technologies: ['React.js', 'Node.js', 'MongoDB', 'Tailwind'],
+        description: 'Website where you can learn math and physics with amazing teachers!',
+        image: '../public/images/korepetycjeOnline-app-preview.png',
+    },
+    {
+        iconIndex: 1,
+        name: 'Allegro clone',
+        technologies: ['Vite.js', 'Node.js', 'MongoDB', 'Tailwind'],
+        description: 'Allegro is a website where you can buy everything and i have built version of it myself',
         image: '../public/images/allegro-app-preview.png',
     },
     {
@@ -58,3 +122,6 @@ const projectData = [
 ];
 
 export default projectData;
+
+
+*/
