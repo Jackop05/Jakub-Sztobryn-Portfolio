23fd28bf0a6969079bfe1598b3daf46f35bbb3c9
Jackop05 <jakubnyrbotzs@gmail.com>
2024-09-03 13:14:53 +0200

    Bug fixes

--- src/components/TechnologiesAndProjects.jsx
+++ src/components/TechnologiesAndProjects.jsx
@@ -77,19 +77,19 @@ const TechnologiesAndProjects = () => {
                 <div className='flex justify-start gap-8 mb-32'>
                     <div ref={(el) => (scaleRefs.current[0] = el)} data-index="0" className='opacity-0 w-[100px] flex flex-col justify-center'>
                         <div className='mx-auto mb-2 rounded-full w-[80px] h-[80px] border-4 border-solid border-neon-blue bg-white flex flex-col justify-center cursor-pointer hover:scale-105 hover:shadow-xl transition-all duration-[300ms]'>
-                            <img src='/images/JS.png' alt="JS icon" className='rounded-full h-[60px] mx-auto' />
+                            <img src='/public/images/JS.png' alt="JS icon" className='rounded-full h-[60px] mx-auto' />
                         </div>
                         <div className='text-center text-xl'>JavaScript</div>
                     </div>
                     <div ref={(el) => (scaleRefs.current[1] = el)} data-index="1" className='opacity-0 w-[100px] flex flex-col justify-center'>
                         <div className='mx-auto mb-2 rounded-full w-[80px] h-[80px] border-4 border-solid border-neon-blue bg-white flex flex-col justify-center cursor-pointer hover:scale-105 hover:shadow-xl transition-all duration-[300ms]'>
-                            <img src='/images/Java.png' alt="JAVA icon" className='rounded-full h-[60px] w-[100px] mx-auto' />
+                            <img src='/public/images/Java.png' alt="JAVA icon" className='rounded-full h-[60px] w-[100px] mx-auto' />
                         </div>
                         <div className='text-center text-xl'>Java</div>
                     </div>
                     <div ref={(el) => (scaleRefs.current[2] = el)} data-index="2" className='opacity-0 w-[100px] flex flex-col justify-center'>
                         <div className='mx-auto mb-2 rounded-full w-[80px] h-[80px] border-4 border-solid border-neon-blue bg-white flex flex-col justify-center cursor-pointer hover:scale-105 hover:shadow-xl transition-all duration-[300ms]'>
-                            <img src='/images/C.png' alt="C icon" className='rounded-full h-[60px] mx-auto' />
+                            <img src='/public/images/C.png' alt="C icon" className='rounded-full h-[60px] mx-auto' />
                         </div>
                         <div className='text-center text-xl'>C</div>
                     </div>
