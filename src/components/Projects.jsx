262cc255c9156ae040232477d2aa7b62fbade7c8
Jackop05 <jakubnyrbotzs@gmail.com>
2025-04-01 13:50:05 +0200

    New bug fixes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -154,14 +154,14 @@ const Projects = () => {
                   </motion.div>
                 )}
                 {project.link && (
-                  <a 
-                    href={`${project.link}`} 
+                  <Link 
+                    to={`${project.link}`} 
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="inline-block mt-4 px-4 py-2 bg-blue-600 hover:bg-blue-500 rounded-lg text-sm font-medium transition-colors"
                   >
                     View Project
-                  </a>
+                  </Link>
                 )}
               </motion.div>
             </VerticalTimelineElement>
